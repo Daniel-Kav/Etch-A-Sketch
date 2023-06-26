cdb51989ca3f6cc2e3abf835f295133c752973f3
daniel-kav <dankavatha01@gmail.com>
2023-06-26 20:43:31 +0300

    a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid

--- main.js
+++ main.js
@@ -18,3 +18,46 @@ for (let i = 0; i < 16 ;i++){
 
 }
 
+let resetButton = document.getElementById("resetButton");
+
+//add event listener to the reset button
+resetButton.addEventListener('click',function(){
+    let gridSize = prompt("Enter number of squares per side :");
+    gridSize = parseInt(gridSize);
+
+    // validate user input and generate new grid if valid
+    if (!isNaN(gridSize)&& gridSize>0 && gridSize <= 100){
+        generateGrid(gridSize);
+    }else{
+        alert("Invalid Input!");
+    }
+
+});
+
+//function to generate the grid based on given size
+function generateGrid(size){
+    let container = document.getElementById("container");
+
+    // remove existing grid
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+        }
+    
+
+    //calculate square size based on the total width
+    const squareSize = 960 / size;
+    // Create new grid squares
+  for (var i = 0; i < size; i++) {
+    for (var j = 0; j < size; j++) {
+      var square = document.createElement("div");
+      square.classList.add("square");
+      square.style.width = squareSize + "px";
+      square.style.height = squareSize + "px";
+      container.appendChild(square);
+    }
+  }
+
+}
+
+// generate the initial grid
+generateGrid(16)
